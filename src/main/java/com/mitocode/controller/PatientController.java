c9e408cdb294d14335ff06ee70df4afaedd4bd27
omarx33 <carlosmorih33@gmail.com>
2023-06-25 21:42:21 -0500

    Control de mensaje de codigo status para la respuesta de los rest, referente video 8

--- src/main/java/com/mitocode/controller/PatientController.java
+++ src/main/java/com/mitocode/controller/PatientController.java
@@ -4,8 +4,12 @@ import com.mitocode.model.Patient;
 import com.mitocode.service.iPatientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,27 +21,44 @@ public class PatientController {
 
 
     @GetMapping
-    public List<Patient> findAll(){
-        return service.findAll();
+    public ResponseEntity<List<Patient>> findAll(){
+
+        List<Patient> list = service.findAll();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Patient findById(@PathVariable("id") Integer id){
-        return service.findById(id);
+    public ResponseEntity<Patient> findById(@PathVariable("id") Integer id){
+        Patient obj = service.findById(id);
+        return new ResponseEntity<>(obj,HttpStatus.OK);
     }
+
+//    @PostMapping
+//    public ResponseEntity<Patient> save(@RequestBody Patient patient){
+//        Patient obj = service.save(patient);
+//        return new ResponseEntity<>(obj, HttpStatus.CREATED);
+//    }
 
     @PostMapping
-    public Patient save(@RequestBody Patient patient){
-       return service.save(patient);
+    public ResponseEntity<Void> save(@RequestBody Patient patient){
+        Patient obj = service.save(patient);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdPatient()).toUri();
+        return ResponseEntity.created(location).build();
+        //esto al terminar de crea debe devolver la url del recurso creado
+        // devolvera esto http://localhost:8080/patients/3
     }
 
+
     @PutMapping
-    public Patient update(@RequestBody Patient patient){
-        return service.update(patient);
+    public ResponseEntity<Patient> update(@RequestBody Patient patient){
+
+        Patient obj = service.update(patient);
+        return new ResponseEntity<>(obj,HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id){
-         service.delete(id);
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
